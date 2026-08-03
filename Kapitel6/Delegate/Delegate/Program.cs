c6b89d0f6a7263239bde7fd25264c406e332f7b9
Tarkin59 <chrisherren59@hotmail.ch>
2026-08-03 13:23:38 +0200

    Add files via upload

---
+++ Kapitel6/Delegate/Delegate/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Delegate
+{
+    internal class Program
+    {
+        //Deklaration eines Delegates
+        delegate int RechenOperation(int a, int b);
+
+        //Methode, passend zum Delegate
+        static int Addieren(int a, int b)
+        {
+            return a + b;
+
+        }
+
+        static void Main(string[] args)
+        {
+            //Zuweisung der Methode an den Delegate
+            RechenOperation op = Addieren;
+
+            //Aufruf der Methode über den Delegate
+            int ergebnis = op(5, 3);
+            
+            Console.WriteLine(ergebnis);
+
+            Console.ReadLine();
+        }
+    }
+}
