2e24ddecf71d882eb46b2e42e61815ef841e84b7
Tarkin59 <chrisherren59@hotmail.ch>
2026-08-03 13:15:43 +0200

    Add files via upload

---
+++ Kapitel6/Interface/Interface/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Interface
+{
+    //Interface deklarieren
+    interface IGeometry
+    {
+        double flaeche_berechnen();
+        double umfang_berechnen();
+    }
+
+    class Quadrat : IGeometry
+    {
+        //Eigene Eigenschaften 
+        double Umfang;
+        double flaeche;
+        double seitenlaenge;
+
+        //Konstruktor
+        public Quadrat(double t_seitenlaenge)
+        {
+            //Zuweisen  der Laenge
+            seitenlaenge = t_seitenlaenge;
+        }
+
+        //Durch Interface geforderte Methode für Flächenberechnung festlegen
+        public double flaeche_berechnen()
+        {
+            return seitenlaenge * seitenlaenge;
+        }
+
+        //Durch Interface geforderte Methode für Umfangberechnung festlegen
+        public double umfang_berechnen()
+        {
+            return 4 * seitenlaenge;
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Quadrat-Objekt erzeugen
+            Quadrat myQuadrat = new Quadrat(5);
+
+            //Fläche ausgeben
+            Console.WriteLine("Fläche: " + myQuadrat.flaeche_berechnen());
+
+            Console.Read();
+        }
+    }
+}
