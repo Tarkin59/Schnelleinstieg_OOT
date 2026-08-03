c241382b40ba2074de14863e52a4a7a14fe3ef85
Tarkin59 <chrisherren59@hotmail.ch>
2026-08-03 17:33:17 +0200

    Add files via upload

---
+++ Kapitel6/Lambda/Lambda/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Lambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Liste von Zahlen
+            List<int> zahlen = new List<int> { 1, 2, 3, 4, 5 };
+
+            // Lambda-Ausdruck zum Filtern: nur gerade Zahlen
+            var geradeZahlen = zahlen.Where(x => x % 2 == 0);
+
+            // Ausgabe
+            foreach (var zahl in geradeZahlen)
+            {
+                Console.WriteLine(zahl);
+            }
+
+        }
+    }
+}
