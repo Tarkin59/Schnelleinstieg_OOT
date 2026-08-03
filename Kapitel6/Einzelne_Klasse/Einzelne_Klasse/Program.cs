c954b9ea2b0596a4c2b7fca0624b653a285e5b5b
Tarkin59 <chrisherren59@hotmail.ch>
2026-08-03 08:39:03 +0200

    Add files via upload

---
+++ Kapitel6/Einzelne_Klasse/Einzelne_Klasse/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.Claims;
+
+namespace Einzelne_Klasse
+{
+    internal class Program
+    {
+        class PKW
+        {
+        //Eigenschaften
+        double laenge;
+        int gewicht = 0;
+        int sitzplaetze = 5;
+
+        //Methoden
+
+        //Konstruktor
+        public PKW(double t_laenge, int t_gewicht, int t_plaetze)
+        {
+
+            //Setzen der Eigenschaften
+            laenge = t_laenge;
+            gewicht = t_gewicht;
+            sitzplaetze = t_plaetze;
+        }
+
+
+        //Methode zum beschleunigen
+        public void beschleunigen()
+        {
+            Console.WriteLine("Ich beschleunige!");
+        }
+
+
+        //Methode zum bremsen
+        public void bremsen()
+        {
+            Console.WriteLine("Ich bremse!");
+        }
+    }
+
+
+        static void Main(string[] args)
+        {
+            //Erstellen eines PKW
+            PKW my_pkw = new PKW(4.5, 1000, 4);
+
+            //Methoden des PKW nutzen
+            my_pkw.beschleunigen();
+            my_pkw.bremsen();
+
+            //Warten
+            Console.Read();
+
+        }
+    }
+}
