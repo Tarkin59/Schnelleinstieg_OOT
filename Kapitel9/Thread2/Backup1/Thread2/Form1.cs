cfd2f6ab993960d6e7b6636437a0ed55f69c7f19
Tarkin59 <chrisherren59@hotmail.ch>
2026-08-06 12:04:23 +0200

    Add files via upload

---
+++ Kapitel9/Thread2/Backup1/Thread2/Form1.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+//Zusätzliche Namespaces
+using System.Threading;
+
+namespace Thread2
+{
+
+    public partial class Form1 : Form
+    {
+        //delegate -> Programmierwurmloch
+        public delegate void MyAnzeigeDelegate(string msg);
+
+        //Klassenglobalen Eigenschaften
+        int summe = 0;
+        int id = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Eigentliche Zugriffsmethode auf die Listbox
+        public void myAnzeige(string msg)
+        {
+            listBox1.Items.Add(msg);
+        }
+
+        //CallBackFunktion
+        public void BerechneMitAnzeige(object anzahl)
+        {
+            lock (this)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(1000);
+                    summe++;
+                    string s = "Thread" + Thread.CurrentThread.Name + " :Summe= " + summe.ToString();
+                    //Zugriff auf ListBox via Delegate
+
+                    listBox1.Invoke(new MyAnzeigeDelegate(myAnzeige), new object[] { s });
+                }
+            }
+        }
+
+        //Hauptmethode
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Thread erzeugen
+            Thread t1 = new Thread(BerechneMitAnzeige);
+            //Name setzen des Thread
+            t1.Name = "T" + id.ToString();
+            id++;
+            //Starten des Thread
+            t1.Start();
+        }
+    }
+}
---
+++ Kapitel9/Thread2/Thread2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Thread2
+{
+    public partial class Form1 : Form
+    {
+        // Gemeinsame Variable, auf die beide Threads zugreifen
+        int summe = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Methode, die von beiden Threads ausgeführt wird
+        public void BerechneMitAnzeige(object anzahl)
+        {
+            // Kritischer Abschnitt: beide Threads greifen auf "summe" zu
+            lock (this)
+            {
+                for (int i = 0; i < (int)anzahl; i++)
+                {
+                    Thread.Sleep(1000);   // simuliert Arbeit (1 Sekunde warten)
+
+                    summe++;              // gemeinsamer Zähler
+                    string s = "Thread " + Thread.CurrentThread.Name +
+                               " : Summe = " + summe.ToString();
+
+                    // ❌ Direkter Zugriff auf die UI aus dem Thread → nicht erlaubt
+                    // listBox1.Items.Add(s);
+
+                    // ✔️ Threadsicherer Zugriff auf die UI
+                    listBox1.Invoke(new Action(() => listBox1.Items.Add(s)));
+                }
+            }
+        }
+
+        // Wird ausgeführt, wenn der Button geklickt wird
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Thread 1 erzeugen
+            Thread t1 = new Thread(BerechneMitAnzeige);
+            t1.Name = "A";
+            t1.Start(5);   // Parameter für die Schleife
+
+            // Thread 2 erzeugen
+            Thread t2 = new Thread(BerechneMitAnzeige);
+            t2.Name = "B";
+            t2.Start(5);   // gleicher Parameter
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Wird beim Laden des Formulars ausgeführt (hier leer)
+        }
+    }
+}
