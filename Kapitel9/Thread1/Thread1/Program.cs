c93a6cac470cb2acf78a150d776ab2adf1243676
Tarkin59 <chrisherren59@hotmail.ch>
2026-08-06 11:56:53 +0200

    Add files via upload

---
+++ Kapitel9/Thread1/Thread1/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        // Erzeugen eines neuen Threads, der die Methode "Berechne" ausführt.
+        // Der Parameter 50 wird als Argument an die Methode übergeben.
+        Thread t1 = new Thread(Berechne);
+        t1.Name = "eins";          // Vergibt einen Namen für Debug-Ausgaben
+        t1.Start(50);              // Startet den Thread mit Parameter 50
+
+        // Zweiter Thread mit anderem Parameter
+        Thread t2 = new Thread(Berechne);
+        t2.Name = "zwei";
+        t2.Start(20);
+
+        // Verhindert, dass das Programm sofort endet
+        Console.ReadLine();
+    }
+
+    // Methode, die von beiden Threads ausgeführt wird.
+    // "object anzahl" ist notwendig, weil Thread.Start() nur object übergeben kann.
+    private static void Berechne(object anzahl)
+    {
+        // Schleife läuft bis zur übergebenen Anzahl
+        for (int i = 0; i < (int)anzahl; i++)
+        {
+            // Debug-Ausgabe zeigt den Threadnamen und den aktuellen Schleifenwert
+            Console.WriteLine("Thread " + Thread.CurrentThread.Name + " arbeitet " + i);
+        }
+
+        // Abschlussmeldung des Threads
+        Debug.Print("Thread " + Thread.CurrentThread.Name + " ist am Ende");
+    }
+}
