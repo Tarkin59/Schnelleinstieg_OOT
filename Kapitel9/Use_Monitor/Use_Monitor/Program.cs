e1727e3c15f4595037bdd8e2f8a16fcba4de9772
Tarkin59 <chrisherren59@hotmail.ch>
2026-08-06 12:11:22 +0200

    Add files via upload

---
+++ Kapitel9/Use_Monitor/Use_Monitor/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+//zusätzliche Namespaces
+using System.Threading;
+using System.Diagnostics;
+
+namespace Use_Monitor
+{
+    class Program
+    {
+        //Eigenschaften
+         static int summe = 0;
+
+        //Internes lock Objekt
+        static object locker = new object();
+
+        //Methode des Threads A
+        static public void BerechneMonitorA(object anzahl)
+        {         
+            for (int i = 0; i < 10; i++)
+            {
+                //Thread macht 100 ms nichts
+                Thread.Sleep(100);
+                //---------------------------------------------
+                //Kritischer Abschnitt startet
+                Monitor.Enter(locker);
+
+                //Hochzählen
+                summe += (int)anzahl;
+                //Nach 5 Durchläufen wird Thread a schlafen gelegt (wartet auf Pulse)
+                if (i == 3) Monitor.Wait(locker);
+                //Ausgabe
+                Console.WriteLine("Thread {0}: i = {1} Summe = {2}", Thread.CurrentThread.Name,i, summe);
+                Monitor.Exit(locker);
+                //Kritischer Abschnitt endet
+                //---------------------------------------------
+        }
+    }
+
+        //Methode des Threads B
+        static public void BerechneMonitorB(object anzahl)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(100);
+                //----------------------------------------------------
+                //Starten des kritischen Abschnitts
+                Monitor.Enter(locker);
+                //Dekremntieren
+                summe -= (int)anzahl;
+                //Nach 8 Durchläufen wird der Wettbewerb wieder eröffnet
+                if (i == 7) Monitor.Pulse(locker);
+                Console.WriteLine("Thread {0}: i = {1} Summe = {2}", Thread.CurrentThread.Name,i, summe);
+                Monitor.Exit(locker);
+                //----------------------------------------------------------
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Instantiieren der Threads
+            Thread A = new Thread(BerechneMonitorA);
+            A.Name = "a";
+            A.Start(1);
+
+            Thread B = new Thread(BerechneMonitorB);
+            B.Name = "b";
+            B.Start(1);
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ Kapitel9/Use_semaphor/Use_semaphor/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Demonstrator für Semaphore
+ * 
+ * C. Herren
+ * 15.04.2025
+ * V2
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Use_semaphor
+{
+    class Program
+    {
+        //Instantiieren eines Semaphors mit 2 Kanälen, 2 davon sind frei
+        static private Semaphore sem = new Semaphore(2, 2);
+
+        //Methode zum Starten von mehreren Threads
+        static public void starte_Threads(int anzahl)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread t = new Thread(Benutze_Semaphor);
+                t.Name = "Thread" + i.ToString();
+                t.Start();
+            }
+        }
+
+        //eigentliche Threadmethode
+        static public void Benutze_Semaphor()
+        {
+            sem.WaitOne();
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(100); 
+                Console.WriteLine("Thread {0}: ", Thread.CurrentThread.Name);
+            }
+            sem.Release();
+        }
+
+        static void Main(string[] args)
+        {
+            //Starten
+            starte_Threads(10);
+            Console.Read();
+        }
+    }
+}
