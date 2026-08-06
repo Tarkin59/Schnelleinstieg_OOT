581950d3906a8b0ce8ad7d5d7d9febe92381230e
Tarkin59 <chrisherren59@hotmail.ch>
2026-08-06 08:05:02 +0200

    Add files via upload

---
+++ Kapitel8/Microservice/SharedModel/SharedModel/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace SharedModel
+{
+    public class Product
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public decimal Price { get; init; }
+
+        public Product(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+    }
+
+        public class Order
+        {
+            public Guid OrderId { get; init; }
+            public string ProductName { get; init; }
+            public decimal Price { get; init; }
+            public DateTime CreatedAt { get; init; }
+
+            public Order(Guid orderId, string productName, decimal price, DateTime createdAt)
+            {
+                OrderId = orderId;
+                ProductName = productName;
+                Price = price;
+                CreatedAt = createdAt;
+            }
+        }
+
+    }
